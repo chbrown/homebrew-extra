b62a5c9c2a2ba810deda0e7215b78fb994b29c67
Christopher Brown <io@henrian.com>
2020-09-18 21:33:47 -0400

    Add sshpass@1.06

---
+++ Formula/sshpass.rb
@@ -0,0 +1,15 @@
+class Sshpass < Formula
+  homepage "https://sourceforge.net/projects/sshpass/"
+  url "https://sourceforge.net/projects/sshpass/files/sshpass/1.06/sshpass-1.06.tar.gz"
+  sha256 "c6324fcee608b99a58f9870157dfa754837f8c48be3df0f5e2f3accf145dee60"
+  license "GPL-2.0"
+
+  def install
+    system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    system "sshpass"
+  end
+end
